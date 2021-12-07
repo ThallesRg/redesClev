ba913cd36c29c42bb773164c5cacf2d9fa1578cf
renatagarib <50972252+renatagarib@users.noreply.github.com>
2021-12-06 21:28:35 -0300

    Add files via upload

---
+++ redesClev-main/aplication/Package.java
@@ -0,0 +1,31 @@
+package aplication;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+public class Package {
+	
+	private String name;
+	private Date date;
+	SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+	String formattedDate;
+
+	public Package(String name) {
+		this.name = name;
+		this.date = Date.from(Instant.now());
+		this.formattedDate = formatter.format(date);
+	}
+	
+	public void updateTime(){
+		date = Date.from(Instant.now());
+		formattedDate = formatter.format(date);
+	}
+
+	@Override
+	public String toString() {
+		return name + " " + formattedDate;
+	}
+
+	
+}
